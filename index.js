b851e0ffaf4926e8d8c88b0c35ecf79af32b471c
Ben Liu <ben.liu@hopebaytech.com>
2016-05-20 15:33:29 +0800

    Add devTools
    Add devTools
    
    [ticket: X]

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import monitoringApp from './reducers'
-import App from './components/App'
 
-let store = createStore(monitoringApp)
+import configureStore from './stores/configureStore'
+import App from './components/App'
+import DevTools from './containers/DevTools'
+
+const store = configureStore()
 
 render(
   <Provider store={store}>
-    <App />
+    <div>
+      <App />
+      <DevTools />
+    </div>
   </Provider>,
   document.getElementById('root')
 )
+
+// if ( process.env.NOVE_ENV !== 'production' ) {
+//   const showDevTools = require('./showDevTools')
+//   showDevTools(store)
+// }
