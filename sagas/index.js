4a939a583c36e8402834bcf64bd023b4caa22d8d
Ben Liu <ben.liu@hopebaytech.com>
2016-05-20 20:34:33 +0800

    Add redux-saga STEP: 0
    Refactor with (tutorial)[https://github.com/abalone0204/redux-thunk-to-saga-tutorial/commit/1a4455b23ce6bc434d17a8c2ebcbf9e80e922be5]
    
    [ticket: X]

---
+++ sagas/index.js
@@ -0,0 +1,8 @@
+import { takeEvery, takeLatest } from 'redux-saga'
+import { call, put } from 'redux-saga/effects'
+
+export default function* rootSaga() {
+    yield [
+
+    ]
+}
--- stores/configureStore.prod.js
+++ stores/configureStore.prod.js
@@ -1,12 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import monitoringApp from '../reducers'
+import createSagaMiddleware from 'redux-saga'
 
-// Middleware you want to use in production:
-const enhancer = applyMiddleware(thunk)
+import monitoringApp from '../reducers'
+import rootSaga from '../sagas'
+
+const saga = createSagaMiddleware()
+
+const enhancer = applyMiddleware(thunk, saga)
 
 export default function configureStore(initialState) {
-  // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
-  // See https://github.com/rackt/redux/releases/tag/v3.1.0
-  return createStore(monitoringApp, initialState, enhancer)
+  const store = createStore(monitoringApp, initialState, enhancer)
+
+  saga.run(rootSaga)
+
+  return store
 }
